f03d54acdadb34f16986619623aeafc9feb3a757
onikd08 <onikd08@gmail.com>
2024-04-08 05:13:35 +0300

    Add logo in navbar and show in the ui

---
+++ components/Logo.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <>
+      <Link href="/">
+        <Image
+          className="max-w-[100px] md:max-w-[165px]"
+          src="/assets/lws_logo.png"
+          alt="Lws"
+          width={100}
+          height={165}
+        />
+      </Link>
+    </>
+  );
+};
+
+export default Logo;
